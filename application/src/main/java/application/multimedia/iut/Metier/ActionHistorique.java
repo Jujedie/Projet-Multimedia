89b38ac292e4f05dc72d99eff4d184712eff62f6
Julien Oyer <oyer.ludovic@gmail.com>
2025-12-16 11:20:10 +0100

    Ajout de la méthode creerActionHistorique pour valider et créer des actions historiques avec des paramètres. Mise à jour de la gestion des actions dans faireAction.

--- application/src/main/java/application/multimedia/iut/Metier/ActionHistorique.java
+++ application/src/main/java/application/multimedia/iut/Metier/ActionHistorique.java
@@ -1,12 +1,67 @@
 package application.multimedia.iut.Metier;
 
+import java.awt.image.BufferedImage;
+
 public class ActionHistorique {
 	
 	private ActionTypeEnum actionType;
 	private Object[]       parametres;
 
-	public ActionHistorique(ActionTypeEnum actionType) {
+	public ActionHistorique creerActionHistorique(ActionTypeEnum actionType, Object[] parametres) {
+		if (parametres.length != actionType.getNbParametres() && actionType.getNbParametres() != -1) {
+			throw new IllegalArgumentException("Le nombre de paramètres ne correspond pas à l'action.");
+		}
+		
+		return new ActionHistorique(actionType, parametres);
+	}
+
+	public ActionHistorique(ActionTypeEnum actionType, Object[] parametres) {
 		this.actionType = actionType;
-		this.parametres = new Object[actionType.getNbParametres()];
+		this.parametres = parametres;
+	}
+
+	public void faireAction(BufferedImage image) {
+		switch (this.actionType) {
+			case TEINTURE:
+				// Implémentation de l'action TEINTURE
+				break;
+			case CONTRASTE:
+				// Implémentation de l'action CONTRASTE
+				break;
+			case LUMIERE:
+				
+				break;
+			case POT_DE_PEINTURE:
+				
+				break;
+
+			case ROTATION:
+				
+				break;
+			case REDIMENSIONNEMENT:
+				
+				break;
+			case COUPER:
+				
+				break;
+			case SYMETRIE_HORIZONTALE:	
+				
+				break;
+			case SYMETRIE_VERTICALE:
+				
+				break;
+
+			case SUPERPOSITION:
+				
+				break;
+			case FUSION:
+				
+				break;
+			case AJOUT_TEXTE_IMAGE:
+				
+				break;
+			default:
+				throw new UnsupportedOperationException("Action non prise en charge : " + this.actionType);
+		}
 	}
 }
