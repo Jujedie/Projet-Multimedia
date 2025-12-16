2432e0d70be6b8b8ced1bbb691fd77d215c5b576
Antoine Lechasles <antoine.lechasles@etu.univ-lehavre.fr>
2025-12-16 13:54:40 +0100

    ajout du header javadoc pour tout les fichiers

--- application/src/main/java/application/multimedia/iut/Main.java
+++ application/src/main/java/application/multimedia/iut/Main.java
@@ -1,7 +1,15 @@
+/**
+ * Classe principale pour lancer l'application de retouche d'images.
+ * 
+ * @author Lechasles Antoine , Martin Ravenel , Julien Oyer
+ * @version 1.0
+ */
+
+
 package application.multimedia.iut;
 
 	import application.multimedia.iut.Vue.PaintFrame;
-import javax.swing.SwingUtilities;
+	import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
