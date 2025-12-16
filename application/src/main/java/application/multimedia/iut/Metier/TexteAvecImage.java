7b86e6efe04fe80243aab492bedfef0e7c1d3835
Antoine Lechasles <antoine.lechasles@etu.univ-lehavre.fr>
2025-12-16 13:54:40 +0100

    ajout du header javadoc pour tout les fichiers

--- application/src/main/java/application/multimedia/iut/Metier/TexteAvecImage.java
+++ application/src/main/java/application/multimedia/iut/Metier/TexteAvecImage.java
@@ -1,3 +1,10 @@
+/**
+ * Classe principale pour lancer l'application de retouche d'images.
+ * 
+ * @author Lechasles Antoine , Martin Ravenel , Julien Oyer
+ * @version 1.0
+ */
+
 package application.multimedia.iut.Metier;
 
 import java.awt.*;
--- application/src/main/java/application/multimedia/iut/Vue/barres/MenuBarBuilder.java
+++ application/src/main/java/application/multimedia/iut/Vue/barres/MenuBarBuilder.java
@@ -1,3 +1,9 @@
+/**
+ * Classe principale pour lancer l'application de retouche d'images.
+ * 
+ * @author Lechasles Antoine , Martin Ravenel , Julien Oyer
+ * @version 1.0
+ */
 package application.multimedia.iut.Vue.barres;
 
 import application.multimedia.iut.Vue.PaintPanel;
