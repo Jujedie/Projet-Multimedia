a8df6c12920262177887da36e4bd297c106d371f
Antoine Lechasles <antoine.lechasles@etu.univ-lehavre.fr>
2025-12-16 10:50:42 +0100

    suppr target

--- application/src/main/java/application/multimedia/iut/Metier/TexteAvecImage.java
+++ application/src/main/java/application/multimedia/iut/Metier/TexteAvecImage.java
@@ -63,111 +63,53 @@ public class TexteAvecImage {
             return null;
         }
         
-        int largeurTexte = calculerLargeurTexte();
-        int hauteurTexte = calculerHauteurTexte();
+        BufferedImage temp = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = temp.createGraphics();
+        g.setFont(police);
+        FontMetrics fm = g.getFontMetrics();
+        int largeur = fm.stringWidth(texte);
+        int hauteur = fm.getHeight();
+        g.dispose();
         
-        BufferedImage image = creerImageVide(largeurTexte, hauteurTexte);
+        BufferedImage image = new BufferedImage(largeur, hauteur, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = image.createGraphics();
-        activerAntialiasing(g2d);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setFont(police);
         
         if (imageFond != null && regionSelectionnee != null) {
-            dessinerTexteAvecImageFond(g2d, largeurTexte, hauteurTexte);
+            g2d.setColor(Color.WHITE);
+            g2d.drawString(texte, 0, fm.getAscent());
+            
+            BufferedImage imageRedim = redimensionnerRegion(largeur, hauteur);
+            if (imageRedim != null) {
+                g2d.setComposite(AlphaComposite.SrcIn);
+                g2d.drawImage(imageRedim, 0, 0, null);
+            }
         } else {
-            dessinerTexteSimple(g2d);
+            g2d.setColor(couleurTexte);
+            g2d.drawString(texte, 0, fm.getAscent());
         }
         
         g2d.dispose();
         return image;
     }
     
-    private int calculerLargeurTexte() {
-        BufferedImage temp = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = temp.createGraphics();
-        g.setFont(police);
-        FontMetrics fm = g.getFontMetrics();
-        int largeur = fm.stringWidth(texte);
-        g.dispose();
-        return largeur;
-    }
-    
-    private int calculerHauteurTexte() {
-        BufferedImage temp = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = temp.createGraphics();
-        g.setFont(police);
-        FontMetrics fm = g.getFontMetrics();
-        int hauteur = fm.getHeight();
-        g.dispose();
-        return hauteur;
-    }
-    
-    private BufferedImage creerImageVide(int largeur, int hauteur) {
-        return new BufferedImage(largeur, hauteur, BufferedImage.TYPE_INT_ARGB);
-    }
-    
-    private void activerAntialiasing(Graphics2D g2d) {
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-    }
-    
-    private void dessinerTexteSimple(Graphics2D g2d) {
-        g2d.setFont(police);
-        g2d.setColor(couleurTexte);
-        
-        FontMetrics fm = g2d.getFontMetrics();
-        int y = fm.getAscent();
-        
-        g2d.drawString(texte, 0, y);
-    }
-    
-    private void dessinerTexteAvecImageFond(Graphics2D g2d, int largeurTexte, int hauteurTexte) {
-        BufferedImage imageFondRedimensionnee = extraireEtRedimensionnerImage(largeurTexte, hauteurTexte);
-        
-        if (imageFondRedimensionnee == null) {
-            dessinerTexteSimple(g2d);
-            return;
-        }
-        
-        dessinerMasqueTexte(g2d);
-        appliquerImageSurTexte(g2d, imageFondRedimensionnee);
-        ajouterContourTexte(g2d);
-    }
-    
-    private BufferedImage extraireEtRedimensionnerImage(int largeurCible, int hauteurCible) {
+    private BufferedImage redimensionnerRegion(int largeur, int hauteur) {
         int x = Math.max(0, regionSelectionnee.x);
         int y = Math.max(0, regionSelectionnee.y);
-        int largeur = Math.min(regionSelectionnee.width, imageFond.getWidth() - x);
-        int hauteur = Math.min(regionSelectionnee.height, imageFond.getHeight() - y);
+        int w = Math.min(regionSelectionnee.width, imageFond.getWidth() - x);
+        int h = Math.min(regionSelectionnee.height, imageFond.getHeight() - y);
         
-        if (largeur <= 0 || hauteur <= 0) {
+        if (w <= 0 || h <= 0) {
             return null;
         }
         
-        BufferedImage region = imageFond.getSubimage(x, y, largeur, hauteur);
-        
-        BufferedImage imageRedim = new BufferedImage(largeurCible, hauteurCible, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = imageRedim.createGraphics();
-        g.drawImage(region, 0, 0, largeurCible, hauteurCible, null);
+        BufferedImage region = imageFond.getSubimage(x, y, w, h);
+        BufferedImage resultat = new BufferedImage(largeur, hauteur, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = resultat.createGraphics();
+        g.drawImage(region, 0, 0, largeur, hauteur, null);
         g.dispose();
-        
-        return imageRedim;
-    }
-    
-    private void dessinerMasqueTexte(Graphics2D g2d) {
-        g2d.setFont(police);
-        g2d.setColor(Color.WHITE);
-        
-        FontMetrics fm = g2d.getFontMetrics();
-        int y = fm.getAscent();
-        
-        g2d.drawString(texte, 0, y);
-    }
-    
-    private void appliquerImageSurTexte(Graphics2D g2d, BufferedImage imageFond) {
-        g2d.setComposite(AlphaComposite.SrcIn);
-        g2d.drawImage(imageFond, 0, 0, null);
-        g2d.setComposite(AlphaComposite.SrcOver);
-    }
-    
-    private void ajouterContourTexte(Graphics2D g2d) {
+        return resultat;
     }
 }
