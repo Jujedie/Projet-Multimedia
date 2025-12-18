560a55c0d43a67d11ed45ef83b17b78283ff8d09
Julien Oyer <oyer.ludovic@gmail.com>
2025-12-18 10:12:23 +0100

    Mise à jour de ActionHistorique et ActionTypeEnum pour améliorer la gestion des actions dans l'historique avec validation des paramètres et ajout de la possibilité d'annulation automatique.

--- application/src/main/java/application/multimedia/iut/Metier/ActionHistorique.java
+++ application/src/main/java/application/multimedia/iut/Metier/ActionHistorique.java
@@ -5,11 +5,20 @@ import java.awt.image.BufferedImage;
 
 import application.multimedia.iut.Controleur;
 
+/**
+ * Représente une action enregistrée dans l'historique pour le système Undo/Redo.
+ */
 public class ActionHistorique {
 	
 	private ActionTypeEnum actionType;
 	private Object[]       parametres;
 
+	/**
+	 * Usine pour créer une ActionHistorique avec validation des paramètres.
+	 * @param actionType Le type d'action.
+	 * @param parametres Les paramètres associés à l'action.
+	 * @return Une nouvelle instance d'ActionHistorique.
+	 */
 	public static ActionHistorique creerActionHistorique(ActionTypeEnum actionType, Object[] parametres) {
 		if (parametres.length != actionType.getNbParametres() && actionType.getNbParametres() != -1) {
 			throw new IllegalArgumentException("Le nombre de paramètres ne correspond pas à l'action.");
@@ -18,11 +27,21 @@ public class ActionHistorique {
 		return new ActionHistorique(actionType, parametres);
 	}
 
+	/**
+	 * Constructeur privé pour forcer l'utilisation de l'usine.
+	 * @param actionType Le type d'action.
+	 * @param parametres Les paramètres associés à l'action.
+	 */
 	public ActionHistorique(ActionTypeEnum actionType, Object[] parametres) {
 		this.actionType = actionType;
 		this.parametres = parametres;
 	}
 
+	/**
+	 * Exécute l'action en utilisant le contrôleur fourni.
+	 * @param image L'image sur laquelle l'action est effectuée.
+	 * @param controleur Le contrôleur central de l'application.
+	 */
 	public void faireAction(BufferedImage image, Controleur controleur) {
 		switch (this.actionType) {
 			case PEINDRE -> {
--- application/src/main/java/application/multimedia/iut/Metier/ActionTypeEnum.java
+++ application/src/main/java/application/multimedia/iut/Metier/ActionTypeEnum.java
@@ -1,17 +1,33 @@
 package application.multimedia.iut.Metier;
 
+/**
+ * Enumération des types d'actions pouvant être enregistrées dans l'historique.
+ */
 public enum ActionTypeEnum {
-	PEINDRE(1),
-	SUPPRESSION_TOTALE(0),
-	DEBUT_PLACEMENT(2);
+	PEINDRE(1, false),
+	SUPPRESSION_TOTALE(0, false),
+	DEBUT_PLACEMENT(2, false),
+	SESSION_PLACEMENT_VALIDE(0, true),
+	;
 
 	private int nbParametres;
+	private boolean autoRetourArriere;
 
-	private ActionTypeEnum(int nbParametres) {
+	/**
+	 * Constructeur de l'énumération.
+	 * @param nbParametres Le nombre de paramètres attendus pour cette action.
+	 * @param autoRetourArriere Indique si l'action peut être automatiquement annulée.
+	 */
+	private ActionTypeEnum(int nbParametres, boolean autoRetourArriere) {
 		this.nbParametres = nbParametres;
+		this.autoRetourArriere = autoRetourArriere;
 	}
 
 	public int getNbParametres() {
 		return nbParametres;
+	}
+
+	public boolean isAutoRetourArriere() {
+		return autoRetourArriere;
 	}
 }
